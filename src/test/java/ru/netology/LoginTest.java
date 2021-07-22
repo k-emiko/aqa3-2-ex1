3566394c5670df4d9e7474ce92e0c6696d4dafd2
k-emiko <62057364+k-emiko@users.noreply.github.com>
2021-07-23 00:23:16 +1100

    now this works with one DockerCompose testcontainer

--- src/test/java/ru/netology/LoginTest.java
+++ src/test/java/ru/netology/LoginTest.java
@@ -6,18 +6,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.Network;
-import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.netology.data.CodeGenerator;
 import ru.netology.data.UserGenerator;
 import ru.netology.page.AuthCodePage;
 import ru.netology.page.LoginPage;
 
-import java.nio.file.Paths;
+import java.io.File;
 import java.sql.SQLException;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -28,38 +24,18 @@ public class LoginTest {
 
     private static String dbUrl;// = "jdbc:mysql://localhost:3306/app"; //in case this needs to be run on localhost with docker-compose
     private static String appUrl;// = "localhost:9999"; //in case this needs to be run on localhost with docker-compose
-    static Network network = Network.newNetwork();
     static String invalidCredential = UserGenerator.generateInvalidCredentials();
 
     @Rule
-    public static MySQLContainer dbCont =
-            new MySQLContainer("mysql:latest");
-    @Rule
-    public static GenericContainer appCont =
-            new GenericContainer(new ImageFromDockerfile("app-deadline")
-                    .withDockerfile(Paths.get("artifacts/deadline/Dockerfile")));
+    public static DockerComposeContainer dcContainer = new DockerComposeContainer(new File("artifacts/docker-compose.yml"))
+            .withExposedService("mysql", 3306)
+            .withExposedService("app-deadline", 9999);
 
     @BeforeAll //disable this method if this needs to be run on localhost with docker-compose
     static void setUpContainersAndBrowser() {
-        dbCont
-                .withDatabaseName("app")
-                .withUsername("app")
-                .withPassword("pass")
-                .withNetwork(network)
-                .withNetworkAliases("mysql")
-                .withFileSystemBind("./artifacts/init/schema.sql", "/docker-entrypoint-initdb.d/schema.sql", BindMode.READ_ONLY)
-                .withExposedPorts(3306)
-                .start();
-        dbUrl = dbCont.getJdbcUrl();
-        appCont
-                .withEnv("TESTCONTAINERS_DB_USER", "app")
-                .withEnv("TESTCONTAINERS_DB_PASS", "pass")
-                .withExposedPorts(9999)
-                .withNetwork(network)
-                .withNetworkAliases("app-deadline")
-                .withCommand("java -jar app-deadline.jar -P:jdbc.url=jdbc:mysql://mysql:3306/app")
-                .start();
-        appUrl = appCont.getHost() + ":" + appCont.getMappedPort(9999);
+        dcContainer.start();
+        dbUrl = "jdbc:mysql://" + dcContainer.getServiceHost("mysql", 3306) + ":" + dcContainer.getServicePort("mysql", 3306) + "/app";
+        appUrl = dcContainer.getServiceHost("app-deadline", 9999) + ":" + dcContainer.getServicePort("app-deadline", 9999);
         Configuration.headless = true;
     }
 
